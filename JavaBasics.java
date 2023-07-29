d84c597139028c9ae7bff59d9310a206950cafc9
AbhinandanGulshetty <139576879+AbhinandanGulshetty@users.noreply.github.com>
2023-07-29 23:34:16 +0530

    Add files via upload
    Simple java code for calculating factorial of numbers

---
+++ JavaBasics.java
@@ -0,0 +1,123 @@
+import java.util.*;
+
+class java {
+
+    public static int factorial(int n){
+        int f = 1;
+   for (int i=1; i<=n; i++){
+    f = f*i;
+      
+   }
+   return f;
+
+   
+
+    }
+       
+     
+    public static void main(String[] args) {
+       System.out.println(factorial(7));
+    }
+}
+
+
+  
+
+            
+            
+                    
+        
+       
+        
+
+
+
+
+
+
+
+
+
+        
+       
+        
+        
+
+      
+        
+        
+
+
+     
+
+
+
+
+
+
+
+
+
+
+
+    
+    
+
+
+ 
+
+
+      
+        
+        
+    
+
+
+
+
+
+
+
+
+ 
+
+  
+
+ 
+ 
+    
+
+
+
+    
+
+    
+//java.util.jar
+
+//boilerplate code 
+//boilerplate code
+//brilerplate code
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+    
+
+
+
+    
+
+    
+
